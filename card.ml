59a23b95ab9a69f01b1e9e29f4f199b0fe625c42
Justin Shillingford <jgs272@cornell.edu>
2017-11-19 14:17:13 -0500

    Laid the foundation for card.ml

---
+++ card.ml
@@ -0,0 +1,14 @@
+type suit
+
+type rank
+
+type card
+
+let init_card rank suit =
+  failwith "Unimplemented"
+
+let rep_ok card =
+  failwith "Unimplemented"
+
+let string_of_card card =
+  failwith "Unimplemented"
