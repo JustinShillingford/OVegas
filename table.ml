ec5e22559f96a1ee1017a823e82ef13b0484831b
Justin Shillingford <jgs272@cornell.edu>
2017-11-19 15:54:40 -0500

    Laid the groundwork for table.ml

---
+++ table.ml
@@ -0,0 +1,19 @@
+open Card
+
+type deck
+
+type pot
+
+type table
+
+let shuffle d =
+  failwith "Unimplemented"
+
+let rep_ok d =
+  failwith "Unimplemented"
+
+let new_deck =
+  failwith "Unimplemented"
+
+let flip_new_card t =
+  failwith "Unimplemented"
