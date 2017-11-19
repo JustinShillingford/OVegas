812077a706d2efe603414b8ef9e70df0207bca0b
Justin Shillingford <jgs272@cornell.edu>
2017-11-19 14:39:44 -0500

    Forgot about the rep_ok. Repimplemented init_card taking that into account 😅

--- card.ml
+++ card.ml
@@ -4,10 +4,8 @@ type rank = int
 
 type card = rank * suit
 
-let init_card (r:rank) s =
-  if ((r >= 1 && r <= 12)
-  && (s = Heart || s = Club || s = Diamond || s = Spade)) then (r, s)
-  else failwith "Not a valid card"
+let init_card (r : rank) (s : suit) =
+  (r, s)
 
 let rep_ok c =
   match c with
