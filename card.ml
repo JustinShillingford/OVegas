44dce177632606114fb119e2129c8198028624f9
Justin Shillingford <jgs272@cornell.edu>
2017-11-19 14:47:52 -0500

    Implemented string_of_card

--- card.ml
+++ card.ml
@@ -14,6 +14,19 @@ let rep_ok c =
     && (s = Heart || s = Club || s = Diamond || s = Spade)) then true else false
   end
 
+let string_of_suit s =
+  match s with
+  | Heart -> "Hearts"
+  | Club -> "Clubs"
+  | Diamond -> "Diamonds"
+  | Spade -> "Spades"
+
 let string_of_card c =
   match c with
-  | (r, s) ->
+  | (r, s) -> begin
+    if ((r >= 2 && r <= 10)) then (string_of_int r) ^ " of " ^ (string_of_suit s)
+    else if (r = 1) then "Ace of " ^ (string_of_suit s)
+    else if (r = 11) then "Jack of " ^ (string_of_suit s)
+    else if (r = 12) then "Queen of " ^ (string_of_suit s)
+    else "King of " ^ (string_of_suit s)
+  end
