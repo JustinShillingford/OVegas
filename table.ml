3f4a5b8a81a76590c23ef6796732dbe13bca1b2d
Johanna Smith-Palliser <jlsp101@gmail.com>
2017-11-20 23:37:00 -0500

    Implemented all of table.ml

--- table.ml
+++ table.ml
@@ -1,5 +1,9 @@
 open Card
 open Player
+open Random
+
+exception InvalidDeck
+exception EmptyDeck
 
 type deck = card list
 
@@ -9,17 +13,73 @@ type table = deck * card list
 
 (*type table = deck * pot * card list *)
 
-let shuffle d =
-  failwith "Unimplemented"
+
+(********* USE REP_OK IN EVERY FUNCTION ANY TIME THERE'S A DECK ***************)
+(* Deck must have 52 cards
+   Deck must have exactly 13 cards of each suit
+   Deck must have exactly 4 cards of each rank
+*)
+
+let rec has13ofSuitHelper d hrt clb dmd spd =
+  match d with
+  | [] -> (spd = 13) && (clb = 13) && (hrt = 13) && (dmd = 13)
+  | (_,s)::t -> begin
+      match s with
+      | Heart -> has13ofSuitHelper t (hrt + 1) clb dmd spd
+      | Club -> has13ofSuitHelper t hrt (clb + 1) dmd spd
+      | Diamond -> has13ofSuitHelper t hrt clb (dmd + 1) spd
+      | Spade -> has13ofSuitHelper t hrt clb dmd (spd + 1)
+  end
+
+let rec has4ofRankHelper d rankArray =
+  match d with
+  | [] -> Array.fold_left (fun acc x -> acc && (x=4)) true rankArray
+  | (r,_)::t -> begin
+      let i = r - 1 in
+      let _ = (rankArray.(i) <- (rankArray.(i) + 1)) in
+      has4ofRankHelper t rankArray
+  end
 
 let rep_ok d =
-  failwith "Unimplemented"
+  let has52 = (List.length d = 52) in
+  let has13ofSuit = has13ofSuitHelper d 0 0 0 0 in
+  let has4ofRank = has4ofRankHelper d (Array.make 13 0) in
+  if (has52 && has13ofSuit && has4ofRank) then d else raise InvalidDeck
 
-let new_deck =
-  failwith "Unimplemented"
+let shuffle d =
+  let rec shuffle_helper acc pull_from_d =
+    if (List.length pull_from_d = 0) && (List.length acc = 52) then acc
+    else begin
+      let n = Random.int (List.length pull_from_d) in
+      let chosen_card = List.nth pull_from_d n in
+      let pull_from_d' = List.filter (fun x -> x <> chosen_card) pull_from_d in
+      shuffle_helper (chosen_card::acc) pull_from_d'
+    end in
+  shuffle_helper [] (rep_ok d)
 
-let flip_new_card t =
-  failwith "Unimplemented"
+let suit_from_n n =
+  match n with
+  | 0 -> Heart
+  | 1 -> Club
+  | 2 -> Diamond
+  | 3 -> Spade
+  | _ -> raise InvalidCard
 
-let make_hand deck =
-  failwith "Unimplemented"
+let new_deck () =
+  let rec new_deck_helper acc nth_card =
+    let suit_of_n = nth_card / 13 in
+    let rank_of_n = nth_card mod 13 in
+    if nth_card == 52 then List.rev acc
+    else new_deck_helper ((rank_of_n + 1, suit_from_n suit_of_n)::acc) (nth_card + 1) in
+  new_deck_helper [] 0
+
+let rec flip_new_card (deck, cards) =
+  match (rep_ok deck) with
+  | h::t -> (t, h::cards)
+  | [] -> raise EmptyDeck
+
+(* Should we be outputting a new table to reflect the deck without those two cards? *)
+let rec make_hand deck =
+  match (rep_ok deck) with
+  | c1::c2::t -> [c1; c2]
+  | _ -> raise EmptyDeck
--- table.mli
+++ table.mli
@@ -4,20 +4,19 @@ open Player
 (* [deck] is a combination of cards where no two cards can be similar. *)
 type deck
 
-(*(* [pot] is an association between a player and the amount of money they
- * contributed to the total pot. *)
-type pot *)
+ (* [pot] is an association between a player and the amount of money they
+ contributed to the total pot. *)
+(* type pot *)
 
 (* [table] is a combination of a deck and middle cards. *)
 type table
 
+(* [rep_ok d] returns [d] if [d] satisfies the representation invariants. *)
+val rep_ok : deck -> deck
+
 (* [shuffle d] is the copy of deck [d] where the order of cards has been
  * randomly changed. *)
 val shuffle : deck -> deck
-
-(* [rep_ok d] returns [d] if [d] satisfies the representation
-   * invariants. *)
-val rep_ok : deck -> deck
 
 (* [new_deck] is the standard 52 cards poker deck. *)
 val new_deck : unit -> deck
