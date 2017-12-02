13ed0106de08958c41633dc85942a98477edccdb
Johanna Smith-Palliser <jlsp101@gmail.com>
2017-12-01 20:15:28 -0500

    Created UI module

---
+++ _build/state.mli
@@ -0,0 +1,18 @@
+(* [state] is a combination of players, a table, a counter keeping track of
+ * turns and rounds of the game. *)
+ type state = {players: Player.player list; round: int; pot:int; table: Table.table}
+ 
+(* [initial_state] is the initial value of a state in a typical game with
+ * the initial value of players and the initial value of a table. *)
+val initial_state : Player.player list -> Table.deck -> state
+
+(* [string_of_state st] is the string representation of the state [st]. *)
+val string_of_state : state -> string
+
+(* [do st p cmd] is the new state of the game after player [p] used the
+ * command [cmd]. *)
+val do' : state -> Player.player -> Command.command -> state
+
+    (* (* [hand_cards st] is the new state where players have been given cards
+ * from the deck on the table. *)
+val hand_cards : state -> state   *)
