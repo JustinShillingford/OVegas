850d7c38f5aa78008bfb7429f51f3fbc72102364
Justin Shillingford <jgs272@cornell.edu>
2017-12-02 23:05:11 -0500

    Removed the 2nd Player Prompt.
    Since we're doing 1 player vs an AI.

--- main.ml
+++ main.ml
@@ -17,7 +17,7 @@ let rec repl st =
         print_endline ("You have just Called.");
         build_table next_state;
         (* print_endline next_state.message; *)
-  
+
         repl next_state
       end
     | Fold -> begin
@@ -78,17 +78,14 @@ let rec repl st =
 
 let playgame () =
   ANSITerminal.(print_string [red] "\nWelcome to OVegas, the OCaml Texas Hold'em!\n");
-  print_endline "Please enter the name of the first player.";
+  print_endline "Please enter your name.";
   print_string "> ";
   let player1_name = read_line () in
-  print_endline "Please enter the name of the second player.";
-  print_string "> ";
-  let player2_name = read_line () in
   (* The part above should allow for the program to obtain the names of the 2
    * players. Need to find way to use these names in st for the repl. *)
   let player1 = init_player player1_name (shuffle (new_deck ())) in
-  let player2 = init_player player2_name (snd player1) in
-  let init_st = initial_state [fst player1; fst player2] (snd player2) in 
+  let player2 = init_player "AI" (snd player1) in
+  let init_st = initial_state [fst player1; fst player2] (snd player2) in
   build_table init_st;
   repl init_st
 
