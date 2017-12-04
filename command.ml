47dcc2d852941057b33d59c6d718e201a3de7715
Johanna Smith-Palliser <jlsp101@gmail.com>
2017-12-03 21:03:35 -0500

    Handles dollar signs entered on bet/raise

--- command.ml
+++ command.ml
@@ -15,7 +15,11 @@ let parse str =
     else
       trimmed_str in
   let second_half =
-    if (String.contains trimmed_str ' ') then
+    if (String.contains trimmed_str '$') then
+      let first_dollar = String.index trimmed_str '$' in
+      let string_len = (String.length trimmed_str - (first_dollar + 1)) in
+      String.sub trimmed_str (first_dollar + 1) string_len
+    else if (String.contains trimmed_str ' ') then
       let string_len = (String.length trimmed_str - (first_space + 1)) in
       String.sub trimmed_str (first_space + 1) string_len
     else
