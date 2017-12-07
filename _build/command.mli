229a480a1056605e8c10e67f021e51a7a64af6e9
Justin Shillingford <jgs272@cornell.edu>
2017-12-06 21:45:33 -0500

    Removing make clean files??

--- _build/command.mli
+++
@@ -1,7 +0,0 @@
-type command = Call | Fold | Bet of int | Check | Raise of int | Quit
-
-(* [parse] takes in string from user input  *)
-val parse : string -> command
-
-(*[cmd_to_string cmd] converts commands to strings *)
-val cmd_to_string : command -> string
