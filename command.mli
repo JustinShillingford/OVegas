423ef2524f2e975d1c8f5043a435c356476b1503
Justin Shillingford <jgs272@cornell.edu>
2017-12-07 15:13:50 -0500

    Command specs have been Beautified

--- command.mli
+++ command.mli
@@ -1,7 +1,7 @@
 type command = Call | Fold | Bet of int | Check | Raise of int | Quit
 
-(* [parse] takes in string from user input  *)
+(* [parse str] is the Command representation of the user's input [str] *)
 val parse : string -> command
 
-(*[cmd_to_string cmd] converts commands to strings *)
+(* [cmd_to_string cmd] is the String representation of a Command [cmd] *)
 val cmd_to_string : command -> string
