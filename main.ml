624970cf17ca21617d0d4954fdcac709b0bd9a1d
Johanna Smith-Palliser <jlsp101@gmail.com>
2017-12-06 20:55:07 -0500

    UI updates

--- main.ml
+++ main.ml
@@ -106,7 +106,15 @@ let rec repl st =
   end
 
 let playgame () =
-  ANSITerminal.(print_string [red] "\nWelcome to OVegas, the OCaml Texas Hold'em!\n");
+  ANSITerminal.(print_string [red] "\nWelcome to OVegas, the OCaml Texas Hold'em!");
+  ANSITerminal.(print_string [cyan] "
+    Texas Hold'em Poker is the world's most popular poker game, both in casinos and online. Here are the key things you need to know:
+    - Every player is dealt two cards, for their eyes only
+    - The dealer spreads five cards - three at once, then another, then another - which can be used by all players to make their best possible five-card hand
+    - Before and after each card(s) is revealed, players take turns to bet. To stay in the hand and see the next card, all players must have put the same amount of chips in the pot as each other
+    - The best poker hand wins the pot
+    If you ever get to less than $20 at the beginning of a round, you automatically lose since you won't be able to make the blind bets.
+    It's a seemingly simple game, but there's tons of strategies to win. Good luck!\n");
   print_endline "Please enter your name.";
   print_string "> ";
   let player1_name = read_line () in
@@ -115,17 +123,8 @@ let playgame () =
   let player1 = init_player player1_name (shuffle (new_deck ())) in
   let player2 = init_player "AI" (snd player1) in
   let init_st = initial_state [fst player1; fst player2] (snd player2) in
-  build_table init_st;
-  ANSITerminal.(print_string [green] "\n\t\t\t      GAME START\n");
-  ANSITerminal.(print_string [cyan] "
-    Texas Hold'em Poker is the world's most popular poker game, both in casinos and online. Here are the key things you need to know:
-    - Every player is dealt two cards, for their eyes only
-    - The dealer spreads five cards - three at once, then another, then another - which can be used by all players to make their best possible five-card hand
-    - Before and after each card(s) is revealed, players take turns to bet. To stay in the hand and see the next card, all players must have put the same amount of chips in the pot as each other
-    - The best poker hand wins the pot
-    If you ever get to less than $20 at the beginning of a round, you automatically lose since you won't be able to make the blind bets.
-    It's a seemingly simple game, but there's tons of strategies to win. Good luck!
-  ");
+  (* build_table init_st; *)
+  ANSITerminal.(print_string [green] "\n\t\t\t      GAME START\n");  
   repl init_st
 
 let () = playgame ()
