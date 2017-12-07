25bae20a1490f085f66f0bd178c6544d9687c871
Johanna Smith-Palliser <jlsp101@gmail.com>
2017-12-06 22:16:16 -0500

    More test cases

--- test.ml
+++ test.ml
@@ -17,15 +17,18 @@ let card_tests = [
 let command_tests = [
   "call1" >:: (fun _ -> assert_equal Call (parse " Call"));
   "call2" >:: (fun _ -> assert_equal Call (parse "call "));
+  "call3" >:: (fun _ -> assert_equal Call (parse "call test some more words"));
   "fold1" >:: (fun _ -> assert_equal Fold (parse "Fold "));
   "fold2" >:: (fun _ -> assert_equal Fold (parse " fold "));
-  "bet1" >:: (fun _ -> assert_equal (Bet(10)) (parse "bet 10"));
+  "fold3" >:: (fun _ -> assert_equal Fold (parse " fold here's some extra text"));  
+  "bet1" >:: (fun _ -> assert_equal (Bet(10)) (parse "bet $10"));
   "bet2" >:: (fun _ -> assert_equal (Bet(40)) (parse " Bet 40"));  
   "quit1" >:: (fun _ -> assert_equal Quit (parse " quit "));
   "quit2" >:: (fun _ -> assert_equal Quit (parse "Quit"));
+  "quit3" >:: (fun _ -> assert_equal Quit (parse "Quit hope this still quits"));  
   "check1" >:: (fun _ -> assert_equal Check (parse " check "));
   "check2" >:: (fun _ -> assert_equal Check (parse "Check"));
-  "raise1" >:: (fun _ -> assert_equal (Raise(30)) (parse "raise 30 "));
+  "raise1" >:: (fun _ -> assert_equal (Raise(30)) (parse "raise $30 "));
   "raise2" >:: (fun _ -> assert_equal (Raise(0)) (parse "raise 0"));
 ]
 
